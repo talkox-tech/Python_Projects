6160f055078d7c44d603c364ae4e77f3b7ef0e08
Hyena <rajmukharjee8@gmail.com>
2026-03-20 14:28:13 +0530

    project02

---
+++ Mini_Project/02_Simple_CalC.py
@@ -0,0 +1,9 @@
+#Simple_Calculator
+print("\n--- Calculator---")
+a = float(input("Enter a number"))
+b = float(input("Enter second number"))
+print("\n---Result--- ")
+print(f"The sum of {a} & {b} = {a+b}")
+print(f"The Difference is {a} & {b} = {a-b}") if a>b else print(f"The Difference is {a} & {b} = {b-a}")
+print(f"The product is {a} & {b} = {a*b}")
+print(f"The Divide is {a/b}") if b!=0 else print("Divide is not possible by 0")
