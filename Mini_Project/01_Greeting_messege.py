35fe0515bb9e8384280a21c7dd5eda9dc981b1bd
Hyena <rajmukharjee8@gmail.com>
2026-03-20 13:59:55 +0530

    day01

---
+++ Mini_Project/01_Greeting_messege.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+current_time = datetime.now()
+hour = current_time.hour
+
+name = input("What is your name?👀\n --> ")
+language = input("What is your favorite programming language?🤔\n -->")
+print(hour)
+
+if(hour>=0 and hour < 12 ):
+    greet = "Good Morning "
+elif(hour>= 12 and hour < 16):
+    greet = "Good Afternoon "
+elif(hour>=16 and hour< 20 ):
+    greet = "Good Evening "
+else :
+    greet = "Good Night "
+
+print("\n-----------------")
+print(f"{greet}! {name}")
+print("Start your {} journey!".format(language))
+ 
+
+
+
+
---
+++ Mini_Project/Learning/01_printing_method.py
@@ -0,0 +1,5 @@
+# printing methods
+name = input()    #you can also take input from user by : input("enter your name: ")
+print(f"Hey {name}! How are you?")
+print("Hey "+ name + " ! how are you?")
+print("Hey {}! how are you?".format(name))
