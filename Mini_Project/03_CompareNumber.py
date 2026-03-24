902713330955cb6ed6d9681e9c70c6604e3d6e79
Hyena <rajmukharjee8@gmail.com>
2026-03-24 12:11:32 +0530

    miniProject:add compare number project

---
+++ Mini_Project/03_CompareNumber.py
@@ -0,0 +1,8 @@
+firstNum = input("Enter first Number: ")
+SecondNum = input("Enter Second Number: ")
+if(firstNum == SecondNum):
+    print("Both are equal")
+elif(firstNum>SecondNum):
+    print(f"{firstNum} is greater than {SecondNum}")
+else:
+    print(f"{SecondNum} is greater than {firstNum})")
